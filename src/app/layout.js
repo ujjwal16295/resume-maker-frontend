7a3adb11fcc73d4bf0d36a61e48eb9dbc864037a
ujjwal patel <newujjwalpatel@gmail.com>
2025-08-03 15:57:19 +0530

    added adsnse

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1028044699975607"
+     crossOrigin="anonymous"></Script>
         {children}
       </body>
     </html>
